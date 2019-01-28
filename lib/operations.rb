a30ba264757dd1ac0dd0efb6ead524d9332bead7
BURAK KOCAK <burakkocak884@hotmail.com>
2019-01-28 18:35:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,6 +11,15 @@ end
 
 
 def not_safe?(speed)
+<<<<<<< HEAD
 speed > 60 ? true : 
 speed < 40 ? true : false
 end
+=======
+speed > 60 ? true :
+speed < 40 ? true : false
+
+end
+	
+
+>>>>>>> 904f308282a56314aec0daee00b4d910d74a9039
